afe523cf96b879247b99443204abac458964786f
kimjenoghwan2991 <vmfosemwjd@likelion.org>
2019-07-11 02:05:09 +0900

    first committ

--- crud/settings.py
+++ crud/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/'
 
+# Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
